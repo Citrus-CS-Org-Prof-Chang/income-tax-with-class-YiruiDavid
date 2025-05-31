6dfc50bc11600b438b189e5c1f0867324840087a
YiruiDavid <ZHAYIRUI223@STUDENT.CITRUSCOLLEGE.EDU>
2025-05-31 14:09:23 -0700

    Update Dummy.java

--- src/main/java/edu/pasadena/cs/cs03b/Dummy.java
+++ src/main/java/edu/pasadena/cs/cs03b/Dummy.java
@@ -1,17 +1,34 @@
 package edu.pasadena.cs.cs03b;
-
+import java.util.Scanner;
 public class Dummy {
 
 	public static void main(String[] args) {
 		// TODO: add you logic here
-	}
+		 int[] salaryBrackets = {0, 20000, 50000, 100000, Integer.MAX_VALUE};
+        double[] taxRates = {0.0, 0.10, 0.20, 0.30, 0.40};
 
-	public static int dummy() {
-		
-		
-		// TOOD: add your logic here
+        TaxTableTools tool = new TaxTableTools();
+        tool.setTaxTable(salaryBrackets, taxRates);
 
-		return 1;
-	}
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter annual income (-1 to exit): ");
+            int income = scanner.nextInt();
+            if (income == -1) break;
 
+            double rate = tool.getTaxRate(income);
+            double tax = income * rate;
+
+            System.out.println("Annual Income: " + income + 
+                               " Tax rate: " + rate +
+                               " Tax to pay: " + tax);
+        }
+        scanner.close();
+    }
+
+    public static int dummy() {
+        return 0;
+    }
 }
+
+	
